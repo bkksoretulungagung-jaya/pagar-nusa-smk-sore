917053bd945f6f45b71ef448418f572c7d30734e
pagarnusasmksore <bkksoretulungagung@gmail.com>
2026-08-16 13:50:36 +0700

    Paksa portal biodata memuat versi Aspel terbaru

--- js/dashboard-shortcuts-v1.js
+++ js/dashboard-shortcuts-v1.js
@@ -108,7 +108,7 @@ function openRegistrationFromShortcut(btn){
 
 function openBiodataFromShortcut(btn){
   pulse(btn);
-  window.location.href='biodata.html?v=5';
+  window.location.href='biodata.html?v=7';
 }
 
 function openCbtFromShortcut(btn){
